4cbe6a1d563912b1e4f5e37cf068c9e014482e4c
SupersushiMega <sascha.rutz@outlook.com>
2022-06-19 23:20:49 +0200

    string function (import working)

--- boot/src/HelloWorld.asm
+++ boot/src/HelloWorld.asm
@@ -1,39 +1,20 @@
-%include "path"
 [org 0x7c00] ;memory offset
-
-	mov ah, 0x0e	;enable tty mode by writing 0x0e into higher part of ax
-	mov al, 'a'	;write newline to al
-	int 0x10	;raise general interrupt for video services
 
 mov bx, antiDupString	;print anti duplication string (see definition of string for details)
 call print
 
-	mov ah, 0x0e	;enable tty mode by writing 0x0e into higher part of ax
-	mov al, 'b'	;write newline to al
-	int 0x10	;raise general interrupt for video services
-
 mov bx, OS_Version
 call print
 
-	mov ah, 0x0e	;enable tty mode by writing 0x0e into higher part of ax
-	mov al, 'c'	;write newline to al
-	int 0x10	;raise general interrupt for video services
-
 call print_NL
-
-	mov ah, 0x0e	;enable tty mode by writing 0x0e into higher part of ax
-	mov al, 'd'	;write newline to al
-	int 0x10	;raise general interrupt for video services
 
 mov bx, msg
 call print
 
-	mov ah, 0x0e	;enable tty mode by writing 0x0e into higher part of ax
-	mov al, 'e'	;write newline to al
-	int 0x10	;raise general interrupt for video services
-
 ; Imfinite loop
 jmp $
+
+%include "string.asm"
 
 ;when testing on pysical hardware some characters were printet multiple times after a specific amount of printed characters.
 ;this will print spaces before to trigger the duplication before the first actual character is printet
@@ -41,7 +22,7 @@ antiDupString:
 	db '              ',0x0d,0
 
 OS_Version:
-	db "SushOS V0.01",0
+	db "SushOS V0.0001",0
 	
 msg:
 	db "Hello World",0
